81f991ed63d58d6764e022f8492365f79e31272a
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-29 17:28:46 -0300

    added nav

---
+++ app/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const Nav = () => {
+  return (
+    <ul className="nav-content">
+      <li>Logo</li>
+      <li>Home</li>
+      <li>About</li>
+    </ul>
+  );
+};
+
+export default Nav;
