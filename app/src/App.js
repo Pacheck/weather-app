706d888850eeba3352675308c6f6779a730fe742
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-28 23:50:28 -0300

    css adjusts

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="app-container">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
