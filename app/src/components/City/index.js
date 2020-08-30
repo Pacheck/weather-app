dbddef653f9ccc8f97ee2e83cf97deaa85fc72ca
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-29 21:05:35 -0300

    added city and request key to api

--- app/src/components/City/index.js
+++ app/src/components/City/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import './index.css';
+// import './index.css';
+
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+
+const API_KEY = 'a872c03613ce4a71b1f265af24764da0';
+
+const API_KEY2 = '679669d1038ec093aab3530bd83cbbce';
 
 const City = () => {
-  return <h3>City</h3>;
+  useEffect(() => {
+    // axiosGetApi();
+  }, []);
+
+  const axiosGetApi = async () => {
+    const response = await Axios.get(
+      `https://api.weatherbit.io/v2.0/current?city=Raleigh&key=${API_KEY}`
+    );
+
+    console.log(response);
+  };
+
+  return <span>city</span>;
 };
 
 export default City;
