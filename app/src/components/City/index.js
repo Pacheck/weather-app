909de6388586bc32f24f79ca5af34ec50ac57131
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-30 05:05:05 -0300

    updated with new states and returnin to tests

--- app/src/components/City/index.js
+++ app/src/components/City/index.js
@@ -8,20 +8,30 @@ const API_KEY = 'a872c03613ce4a71b1f265af24764da0';
 
 const API_KEY2 = '679669d1038ec093aab3530bd83cbbce';
 
-const City = () => {
+const City = (props) => {
+  const [cityName, setCityName] = useState('');
+  const [temperature, setTemperature] = useState('');
+
   useEffect(() => {
-    // axiosGetApi();
+    axiosGetApi();
   }, []);
 
   const axiosGetApi = async () => {
     const response = await Axios.get(
-      `https://api.weatherbit.io/v2.0/current?city=Raleigh&key=${API_KEY}`
+      `https://api.weatherbit.io/v2.0/current?city=${props.citie}&key=${API_KEY}`
     );
 
+    setCityName(response.data.data[0].city_name);
+    setTemperature(response.data.data[0].temp);
     console.log(response);
   };
 
-  return <span>city</span>;
+  return (
+    <div>
+      <span>City name: {cityName}</span>
+      <span>Temp : {temperature} ºC</span>
+    </div>
+  );
 };
 
 export default City;
