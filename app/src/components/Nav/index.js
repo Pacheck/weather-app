cda76762cd4fb60d911508a8a28e89e713861d05
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-29 21:06:05 -0300

    adjusts nav comp

--- app/src/components/Nav/index.js
+++ app/src/components/Nav/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './index.css';
 
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
   return (
     <ul className="nav-content">
-      <li>Logo</li>
-      <li>Home</li>
-      <li>About</li>
+      <li>
+        <Link to="/Logo">Logo</Link>
+      </li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/About">About</Link>
+      </li>
     </ul>
   );
 };
