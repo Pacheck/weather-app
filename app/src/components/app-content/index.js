5841c084c69f63dc90f53bb6c87d40830ce5c5d7
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-30 05:04:46 -0300

    updated app component

--- app/src/components/app-content/index.js
+++ app/src/components/app-content/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 
-import { BrowserRouter, Link, Switch, Route, Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 //components
 import Search from '../Search';
