1564f3489c12986659b493f062e26156c580ec91
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-29 21:05:16 -0300

    adjust routes and css

--- app/src/components/app-content/index.js
+++ app/src/components/app-content/index.js
@@ -6,17 +6,21 @@ import { BrowserRouter, Link, Switch, Route, Router } from 'react-router-dom';
 //components
 import Search from '../Search';
 import Nav from '../Nav';
+import About from '../About';
+import Home from '../Home';
 
 const AppContent = () => {
   return (
     <div className="app-content">
-      <Nav />
       <BrowserRouter>
-        <Switch>
-          {/* <Link to="/Search">Search city</Link> */}
-          <Route path="/" exact />
-          <Route path="/Search" component={Search} />
-        </Switch>
+        <Nav />
+        <div className="app-container">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/Search" exact component={Search} />
+            <Route path="/About" component={About} />
+          </Switch>
+        </div>
       </BrowserRouter>
     </div>
   );
