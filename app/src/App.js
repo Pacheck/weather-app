e3e55102b9c6c9ae86178a17efcc58c194ede2df
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-09-04 14:15:58 -0300

    removing reducers scrap

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './actions';
 
 import './App.css';
 import AppContent from './components/app-content';
 
 function App() {
-  const counter = useSelector((state) => state.counter);
-  const isLogged = useSelector((state) => state.isLogged);
-
-  const dispatch = useDispatch();
-
   return (
     <div className="app-container">
-      <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(increment(2))}>+</button>
-      <button onClick={() => dispatch(decrement(2))}>-</button>
-      {isLogged ? <h2>IsLogged!</h2> : <p>Not logged</p>}
-      {/* <AppContent /> */}
+      <AppContent />
     </div>
   );
 }
