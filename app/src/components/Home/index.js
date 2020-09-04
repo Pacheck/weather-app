7b42a80b786d954ce5eef4ef198987b6c2dc7ea0
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-09-04 17:26:24 -0300

    added logic to store favorites in home

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect } from 'react';
 
 const Home = () => {
-  return <h2>Home</h2>;
+  useEffect(() => {});
+
+  async function postIt() {
+    await Axios.post('http://localhost:8080/produtos/', {
+      nome: 'kuat',
+      descricao: 'refrigerante de guaraná 2L',
+      preco: 7.0,
+      categoria_id: 2,
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <h2>
+      <button onClick={() => postIt()}>Pesquisar</button>
+    </h2>
+  );
 };
 
 export default Home;
