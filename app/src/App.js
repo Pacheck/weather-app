967c76f73c9e368856b23968b02dbeb660ccfd64
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-09-03 23:51:47 -0300

    initializing reducer

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 import './App.css';
-
 import AppContent from './components/app-content';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+
+  const dispatch = useDispatch();
+
   return (
     <div className="app-container">
-      <AppContent />
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(2))}>+</button>
+      <button onClick={() => dispatch(decrement(2))}>-</button>
+      {isLogged ? <h2>IsLogged!</h2> : <p>Not logged</p>}
+      {/* <AppContent /> */}
     </div>
   );
 }
