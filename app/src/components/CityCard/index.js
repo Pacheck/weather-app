81ca3b6afe81d50d1d3e9beee0076bd657b84b84
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-09-06 19:42:21 -0300

    added new card settings

--- app/src/components/CityCard/index.js
+++ app/src/components/CityCard/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+// import Axios from 'axios';
+
 import './index.css';
 
-const CityCard = () => {
-  return <h1>City Card</h1>;
+const CityCard = (props) => {
+  return (
+    <div>
+      <h2>
+        {props.name}, {props.country}
+      </h2>
+      <img src={props.icon} sizes={'5px'} />
+    </div>
+  );
 };
 
 export default CityCard;
