7ab0377e8ff1a5b6428599a6cc80a267699ab032
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-09-07 20:56:35 -0300

    added switch component and update database

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -4,13 +4,19 @@ import React, { useEffect, useState } from 'react';
 import './index.css';
 
 import CityCard from '../CityCard';
+import City from '../City';
 
 const Home = () => {
   const [cityList, setCityList] = useState([]);
+  const [showCity, setShowCity] = useState('');
+  const [cityID, setCityID] = useState('');
+  const [update, setUpdate] = useState(false);
 
   useEffect(() => {
     getFavoritesFromDatabase();
-  }, []);
+  }, [update]);
+
+  console.log(update);
 
   async function getFavoritesFromDatabase() {
     await Axios.get('http://localhost:8080/favoritos')
@@ -22,24 +28,51 @@ const Home = () => {
       );
   }
 
+  function handleSearchCity() {
+    console.log({
+      handlerMessage: 'new showCity handler passthrough',
+    });
+    setShowCity('');
+    setCityID('');
+  }
+
   console.log(cityList);
 
   return (
     <div className="home-container">
       <h1>Weather App</h1>
-      <ul>
-        {cityList.map((city) => {
-          return (
-            <li key={city.id}>
-              <CityCard
-                name={city.cityName}
-                country={city.countryCode}
-                icon={city.icon}
-              />
-            </li>
-          );
-        })}
-      </ul>
+
+      {showCity && (
+        <City
+          citie={showCity}
+          citieID={cityID}
+          handleSearchCity={handleSearchCity}
+          updateHomeView={setUpdate}
+          homeView={update}
+        />
+      )}
+
+      {!showCity && (
+        <ul>
+          {cityList.map((city) => {
+            return (
+              <li
+                key={city.id}
+                onClick={() => {
+                  setShowCity(city.cityName);
+                  setCityID(city.id);
+                }}
+              >
+                <CityCard
+                  name={city.cityName}
+                  country={city.countryCode}
+                  icon={city.icon}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
