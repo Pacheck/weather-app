30dd9b36c0e498ae85edf5595e5ece7425c7df28
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-09-14 21:40:38 -0300

    about text

--- app/src/components/About/index.js
+++ app/src/components/About/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const About = () => {
-  return <h2>About</h2>;
+  return (
+    <div>
+      <h2>Weather App</h2>
+      <span></span>
+    </div>
+  );
 };
 
 export default About;
